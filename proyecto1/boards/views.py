8c487c0ec4797abacb45faf3e9a696ae02902a74
josehermosilla <jose.hermosillaa@usach.cl>
2023-06-19 20:31:27 -0400

    se realiza fecha.html, para obtener fecha actual

--- proyecto1/boards/views.py
+++ proyecto1/boards/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 #importar vista basada en clase
 from django.views.generic import TemplateView
+#libreria para el manejo de las fechas
+import datetime
+
+
 # Create your views here.
 def index_view(request):
     return HttpResponse("<h1>Hola Mundo, desde la app boards</h1>")
@@ -9,3 +13,9 @@ def index_view(request):
 class IndexView(TemplateView):
     template_name = "boards/index.html"  #buscamos dentro de la carpeta templates(de boards),
     # la carpeta boards y dentro un archivo llamado index.html
+
+def get_date_view(request):
+    fecha_actual = datetime.datetime.now()
+    context = {'fecha': fecha_actual}
+    return render(request,'boards/fecha.html', context)
+        #  render(request, template, datos(context[diccionario]))
